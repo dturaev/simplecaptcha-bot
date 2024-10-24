3db1159bfec08e86838d405b46f6e15f6099288c
DT <d.turaev@gmail.com>
2024-10-24 18:38:07 +0200

    ...

--- eva/handlers/chat/__init__.py
+++ eva/handlers/chat/__init__.py
@@ -3,11 +3,14 @@ from aiogram import Router
 from eva.filters.chat_type import ChatType
 
 from .chat_join_request import router as chat_join_request_router
+from .chat_member import router as chat_member_router
 from .my_chat_member import router as my_chat_member_router
 
 router = Router()
 router.chat_join_request.filter(ChatType(types={"group", "supergroup"}))
 router.my_chat_member.filter(ChatType(types={"group", "supergroup"}))
+router.chat_member.filter(ChatType(types={"group", "supergroup"}))
 
 router.include_router(chat_join_request_router)
 router.include_router(my_chat_member_router)
+router.include_router(chat_member_router)
--- eva/handlers/chat/chat_member.py
+++ eva/handlers/chat/chat_member.py
@@ -13,21 +13,21 @@ LEAVE_TRANSITION = ~JOIN_TRANSITION
 @router.chat_member(
     ChatMemberUpdatedFilter(member_status_changed=JOIN_TRANSITION),
 )
-async def member_joined(event: types.ChatMemberUpdated, bot: Bot) -> None:
-    user = event.new_chat_member.user
+async def member_joined(update: types.ChatMemberUpdated, bot: Bot) -> None:
+    user = update.new_chat_member.user
     logger.warning(f"User {user_repr(user)} joined!")
     text = (
         f"{user.mention_html(user.first_name)}, привет и "
-        "филь шпас ☀️ если что, у нас есть #правила"
+        "филь шпас ☀️ при вопросах загляни в #правила"
     )
-    # await bot.send_message(chat_id=update.chat.id, text=text)
+    await bot.send_message(chat_id=update.chat.id, text=text)
 
 
 @router.chat_member(
     ChatMemberUpdatedFilter(member_status_changed=LEAVE_TRANSITION),
 )
-async def member_left(event: types.ChatMemberUpdated, bot: Bot) -> None:
-    user = event.old_chat_member.user
+async def member_left(update: types.ChatMemberUpdated, bot: Bot) -> None:
+    user = update.old_chat_member.user
     logger.warning(f"User {user_repr(user)} left!")
-    # text = "Привет и хэв фан ☀️ если что, есть и #правила"
+    # text = "Пока"
     # await bot.send_message(chat_id=update.chat.id, text=text)
